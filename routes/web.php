b70868dbe3faf4cae435ceaebbce3f08f4dcb395
tareqsifat <mdtareq.me@gmail.com>
2021-12-23 20:59:55 +0600

    websocket

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Route::group(['prefix' => 'internal', 'middleware' => ['jwt.verify','admin']], function(){
+//     Route::get('sockets/serve', function(){
+//         \Illuminate\Support\Facades\Artisan::call('websockets:serve');
+//     });
+// });
+
 Route::get('/', function () {
     return view('welcome');
 });
